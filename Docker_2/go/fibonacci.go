5839b699a78da3bf054ce25c24eae0ff5f7a9719
Siberodi <99750883+Siberodi@users.noreply.github.com>
2025-02-16 20:42:19 -0500

    Update fibonacci.go

--- Docker_2/go/fibonacci.go
+++ Docker_2/go/fibonacci.go
@@ -15,8 +15,9 @@ func fibonacci(n int) int {
 
 func main() {
     start := time.Now()
-    result := fibonacci(10000)
+    result := fibonacci(10000) // Variable `result` ahora es usada
     elapsed := time.Since(start)
 
-    fmt.Println(elapsed.Milliseconds())
+    fmt.Println(elapsed.Milliseconds()) // Muestra el tiempo en ms
+    _ = result // Se usa `result` para evitar error de variable no utilizada
 }
